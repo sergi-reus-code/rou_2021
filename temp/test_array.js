9eff06a9ed3bc3a352414772fb76f8fd0a66a944
sergi-reus-code <sergi@reusconstruccions.com>
2021-09-23 08:26:00 +0200

    fghjf

--- temp/test_array.js
+++ temp/test_array.js
@@ -1,7 +1,7 @@
-let rectan = require('./db_manager');
-let retang = new Rectangle(10,10);
+//let rectan = require('./db_manager');
+//let retang = new Rectangle(10,10);
 
-//let test_array = [[0,0,0,"A","B","C","D","E","G"]];
+let test_array = [];
 let test_array1 = [];
 let test_array2 = [];
 let test_array3 = [];
@@ -27,28 +27,27 @@ let test_array20 = [];
 
 function someOtherFunction(){
 
-for (let index = 0; index < 225; index++) {       //25000000    90000000
-    test_array1.push("A");
-    test_array2.push("A");
-    test_array3.push("A");
-    test_array4.push("A");
-    test_array5.push("A");
-    //test_array6.push("A");
-    //test_array7.push("A");
-    //test_array8.push("A");
-    //test_array9.push("A");
-    //test_array10.push("A");
-    //test_array11.push("A");
-    //test_array12.push("A");
-    //test_array13.push("A");
-    //test_array14.push("A");
-    //test_array15.push("A");
-    //test_array16.push("A");
-    //test_array17.push("A");
-    //test_array18.push("A");
-    //test_array19.push("A");
-    //test_array20.push("A");
-   
+for (let index = 0; index < 10000000; index++) {       //25000000    90000000
+    test_array1.push(0,0,0,0,512,512);
+    test_array2.push(0,0,0,0,512,512);
+    test_array3.push(0,0,0,0,512,512);
+    test_array4.push(0,0,0,0,512,512);
+    test_array5.push(0,0,0,0,512,512);
+    test_array6.push(0,0,0,0,512,512);
+    test_array7.push(0,0,0,0,512,512);
+    test_array8.push(0,0,0,0,512,512);
+    test_array9.push(0,0,0,0,512,512);
+    test_array10.push(0,0,0,0,512,512);
+    test_array11.push(0,0,0,0,512,512);
+    test_array12.push(0,0,0,0,512,512);
+    test_array13.push(0,0,0,0,512,512);
+    test_array14.push(0,0,0,0,512,512);
+    test_array15.push(0,0,0,0,512,512);
+    test_array16.push(0,0,0,0,512,512);
+    test_array17.push(0,0,0,0,512,512);
+    test_array18.push(0,0,0,0,512,512);
+    test_array19.push(0,0,0,0,512,512);
+    test_array20.push(0,0,0,0,512,512);
 }
 }
 
@@ -82,4 +81,4 @@ someFunction();
 
 
 
-console.log(test_array2.length);
+console.log(test_array1.length);
