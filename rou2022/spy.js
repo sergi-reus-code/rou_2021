eabc59e40c4806da139843e29245636c6b30c327
sergi-reus-code <sergi@reusconstruccions.com>
2021-10-18 18:57:39 +0200

    rghj

--- rou2022/spy.js
+++ rou2022/spy.js
@@ -1,8 +1,7 @@
 const prompt = require('prompt-sync')({sigint: true});
 
-//const {randomInt} = require ("./utils/spy_utils");
-
-//const {update_combi_pool, get_best_bet, get_chk} = require ("./utils/combi_master");
+const {randomInt} = require ("./utils/spy_utils");
+const {update_combi_pool, get_best_bet, get_chk} = require ("./utils/combi_master");
 
 var combi_totales = 0;
 var last_apuesta = [];
@@ -33,12 +32,12 @@ function main_loop(){
     /**
      *  Automatic Input
      */
-    //var spin = randomInt(0,36);
+    var spin = randomInt(0,36);
 
     /**
      *  Manual Input
      */
-    var spin = prompt('Introduce Spin: ');
+    //var spin = prompt('Introduce Spin: ');
 
 
 
@@ -61,23 +60,23 @@ function main_loop(){
 
     update_combi_pool([combi_totales, spin]);
 
-    last_apuesta = current_apuesta;
+    
     let current_apuesta = get_best_bet();
 
     // poner por pantalla
 
-    console.log(apuesta + " - " + Number(get_chk(apuesta)));
+    console.log(current_apuesta + " - " + Number(get_chk(current_apuesta)));
 
+/*
 
-
-    if (apuesta != false ) {
+    if (curreapuesta != false ) {
 
     let bet_si_no = prompt('Quieres apostar: Introduce . + Intro ---->  ');
         console.log(bet_si_no);
         return;
     }
-
+*/
 }
   
 
-setInterval(main_loop, 2000);
+setInterval(main_loop, 500);
