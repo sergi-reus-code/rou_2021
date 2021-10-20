01f5e865122bf2954c97fbb70fce0379e1f01685
sergi-reus-code <sergi@reusconstruccions.com>
2021-10-20 19:26:13 +0200

    u9

--- rou2022/utils/coord_888.js
+++ rou2022/utils/coord_888.js
@@ -1,6 +1,87 @@
-var n0 = [135,558];
+/**
+ * Coordenadas para poner ventana como queremos
+ */
+//Silenciar
+var window1 = [135,558];
+var window2 = [135,558];
+var window3 = [135,558];
+var window4 = [135,558];
+//Video
+var window5 = [135,558];
+var window6 = [135,558];
+var window7 = [135,558];
+var window8 = [135,558];
+var window9 = [135,558];
+//Otros
+var window5 = [135,558];
+var window6 = [135,558];
+var window7 = [135,558];
+var window8 = [135,558];
+var window9 = [135,558];
 
+/**
+ * Coordenadas para detectar numeros
+ */
+ var d1 = [135,558,135,558];
+ var d2 = [135,558,135,558];
+ var dh = [135,558,135,558];
+ var dtirada = [135,558,135,558];
+ var cnegro1
+ var cnegro2
+ 
+ 
+ 
+/**
+ * Coordenadas para "clikear" apuestas
+ */
 
+//Numeros
+var n0 = [135,558];var n10 = [135,558];var n20 = [135,558];var n30 = [135,558];
+var n1 = [135,558];var n11 = [135,558];var n21 = [135,558];var n31 = [135,558];
+var n2 = [135,558];var n12 = [135,558];var n22 = [135,558];var n32 = [135,558];
+var n3 = [135,558];var n13 = [135,558];var n23 = [135,558];var n33 = [135,558];
+var n4 = [135,558];var n14 = [135,558];var n24 = [135,558];var n34 = [135,558];
+var n5 = [135,558];var n15 = [135,558];var n25 = [135,558];var n35 = [135,558];
+var n6 = [135,558];var n16 = [135,558];var n26 = [135,558];var n36 = [135,558];
+var n7 = [135,558];var n17 = [135,558];var n27 = [135,558];
+var n8 = [135,558];var n18 = [135,558];var n28 = [135,558];
+var n9 = [135,558];var n19 = [135,558];var n29 = [135,558];
+
+//Apuestas
+var a01 = [135,558];
+var a1 = [135,558];
+var a2 = [135,558];
+var a5 = [135,558];
+var a25 = [135,558];
+var a100 = [135,558];
+var doblar = [135,558];
+var deshacer = [135,558];
+
+//Apuestas Mesa
+var par = [135,558];var impar = [135,558];
+var rojo = [135,558];var negro = [135,558];
+var falta = [135,558];var pasa = [135,558];
+var d1 = [135,558];var d2 = [135,558];var d3 = [135,558];
+var c1 = [135,558];var c2 = [135,558];var c3 = [135,558];
+
+//Apuestas mesa especiales
+var tier = [135,558];
+var orphelins = [135,558];
+var voisins = [135,558];
+var zero = [135,558];
+
+//Apuestas mesa especiales vecinos numero
+var vplus = [135,558];var vminus = [135,558];
+var nv0 = [135,558];var nv10 = [135,558];var nv20 = [135,558];var nv30 = [135,558];
+var nv1 = [135,558];var nv11 = [135,558];var nv21 = [135,558];var nv31 = [135,558];
+var nv2 = [135,558];var nv12 = [135,558];var nv22 = [135,558];var nv32 = [135,558];
+var nv3 = [135,558];var nv13 = [135,558];var nv23 = [135,558];var nv33 = [135,558];
+var nv4 = [135,558];var nv14 = [135,558];var nv24 = [135,558];var nv34 = [135,558];
+var nv5 = [135,558];var nv15 = [135,558];var nv25 = [135,558];var nv35 = [135,558];
+var nv6 = [135,558];var nv16 = [135,558];var nv26 = [135,558];var nv36 = [135,558];
+var nv7 = [135,558];var nv17 = [135,558];var nv27 = [135,558];
+var nv8 = [135,558];var nv18 = [135,558];var nv28 = [135,558];
+var nv9 = [135,558];var nv19 = [135,558];var nv29 = [135,558];
 
 
 
