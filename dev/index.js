c98c7e70db0b89b4a86ee676f0e016c6f6db780b
Sergi Reus <sergireus28@gmail.com>
2021-12-05 20:44:38 +0100

    fyj

---
+++ dev/index.js
@@ -0,0 +1,18 @@
+const cron = require('node-cron')
+var child_process = require('child_process');
+const utils = require("./utils/utils");
+var path = require("path");
+
+
+function open_init() {
+    
+    child_process.exec(`start \"exec\" cmd.exe /c node ${__dirname}\\init888.js ${process.argv[3]}`);
+ 
+}
+
+
+
+
+//cron.schedule('*/1 * * * *' ,() => {
+   open_init();
+//})
